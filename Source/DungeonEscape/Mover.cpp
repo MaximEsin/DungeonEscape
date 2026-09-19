95464e579d479cb009713e4b79ca3a7fc5d49219
Maxim Esin <maximesin21@gmail.com>
2026-09-19 12:06:32 +0400

    feat: add example of using pointers

--- Source/DungeonEscape/Mover.cpp
+++ Source/DungeonEscape/Mover.cpp
@@ -19,6 +19,30 @@ void UMover::BeginPlay()
 {
 	Super::BeginPlay();
 
+	float MyFloat = 10.0f;
+	float* MyFloatPointer = &MyFloat;
+
+	float Result = *MyFloatPointer + 10.0f;
+	UE_LOG(LogTemp, Display, TEXT("Result: %f"), Result);
+
+	*MyFloatPointer = 30.0f;
+	UE_LOG(LogTemp, Display, TEXT("MyFloat: %f"), MyFloat);
+	UE_LOG(LogTemp, Display, TEXT("MyFloatPointer: %f"), *MyFloatPointer);
+	UE_LOG(LogTemp, Display, TEXT("MyFloatPointer: %p"), MyFloatPointer);
+	UE_LOG(LogTemp, Display, TEXT("MyFloatPointer: %p"), &MyFloat);
+
+	// ------
+
+	FVector MyVector(1.0f, 2.0f, 3.0f);
+	FVector* MyVectorPointer = &MyVector;
+
+	MyVectorPointer->X = 10.0f;
+	MyVectorPointer->Y = 20.0f;
+	MyVectorPointer->Z = 30.0f;
+
+	UE_LOG(LogTemp, Display, TEXT("MyVector: %s"), *MyVector.ToString());
+	UE_LOG(LogTemp, Display, TEXT("MyVectorPointer: %p"), MyVectorPointer);
+
 }
 
 
